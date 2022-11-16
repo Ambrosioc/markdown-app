92a4cddc3442d9020a9503916391793db0bf71ba
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-11-16 15:39:57 +0100

    finish projet markdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { sampleText } from "./sampleText";
+import { marked } from "marked";
 
 function App() {
+  const [text, setText] = useState(sampleText);
+
+  const handleChange = (event) => {
+    const text = event.target.value;
+    setText(text);
+  };
+
+  const renderText = (text) => marked(text, { sanitize: true });
+  useEffect(() => {
+    console.log("I have been mounted");
+    const text = localStorage.getItem("text");
+    setText(text);
+  }, []);
+
+  useEffect(() => {
+    console.log("I will only run if my deps change: ");
+    localStorage.setItem("text", text);
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea
+            onChange={handleChange}
+            value={text}
+            className="form-control"
+            rows="35"
+          >
+            {sampleText}
+          </textarea>
+        </div>
+        <div className="col-sm-6">
+          <div dangerouslySetInnerHTML={{ __html: renderText(text) }} />
+        </div>
+      </div>
     </div>
   );
 }
